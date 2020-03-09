92516a3f4ef22f109326194f02b867c809cebcf3
dave <lklone2005@gmail.com>
2020-03-09 23:24:31 +0900

    MySQL in main.js

--- main.js
+++ main.js
@@ -3,8 +3,29 @@ const fs = require('fs');
 const url = require('url');
 const template = require('./lib/template');
 
+const mysql_dbc = require('./db_con')();
+const getDb = mysql_dbc.init();
+mysql_dbc.test_open(getDb);      // MySQL server connect
+
+
 const app = http.createServer(       //create server
   function(request,response) {   
+    
+    /*
+    getDb.query(`SELECT * FROM queto_src`, function(error, quetos) {
+      if(error) {throw error};
+      const id = quetos.id;
+      const title = quetos.queto;
+    });
+    */
+
+    getDb.query(`SELECT * FROM queto_src WHERE id = ?`,[], function(error, quetos) {
+      if(error) {throw error};
+      const id = quetos.id;
+      const title = quetos.queto;
+    });
+
+
     var url = request.url;
       if(request.url == '/'){
         url = '/index.html';
--- queto.js
+++ queto.js
@@ -16,3 +16,22 @@ function init() {
 }
 
 init();
+
+
+/*
+<div id="queto">
+    <div id="container">
+        <form class="queto_form">
+            <input type="button" value="button"></input>
+        </form>
+        <h2 class="queto">This is Queto</h2>
+    </div>
+</div>
+main.js에서 랜덤으로 숫자를 발행하여 db에서 가져올때부터 하나식 가져오게?
+-> '클릭'하는 이벤트가 queto.js에 들어가있다. 서버에서 request는 서버를 redirection할때마다 데이터를 갖고오겠지만 '클릭'이라는 이벤트를 갖지는 못한다. 
+
+
+아니면 queto.js에서 함수로 랜덤 숫자를 만들고 
+-> 데이터가 필요하다. 데이터를 main.js에서 받아와야한다. 
+*/  
+
